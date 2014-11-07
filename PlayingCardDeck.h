e7d6ca4a69ac5547fd01148677db17975d9f68f0
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2014-11-07 16:24:24 -0500

    Add Playing Card Deck Object

---
+++ PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Alexander Sapountzis on 11/7/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
