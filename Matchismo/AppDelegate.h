a1942c097ce57072393f59569ba8a4bf62dea72d
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2014-11-07 11:41:48 -0500

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Alexander Sapountzis on 11/7/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Alexander Sapountzis on 11/7/14.
+//  Copyright (c) 2014 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
