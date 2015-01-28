b121a0e13920225b22beeae8cb7e06267c76d238
Alexander Sapountzis <alexander.sapountzis@gmail.com>
2015-01-27 19:28:51 -0500

    Task #2
    Create a button that will reset the score and game, with new cards in the deck.

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -14,6 +14,7 @@
 @property (strong, nonatomic) CardMatchingGame *game;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startGame;
 @end
 
 @implementation ViewController
@@ -43,6 +44,16 @@
 - (IBAction)touchCardButton:(UIButton *)sender {
     int chooseButtonIndex = [self.cardButtons indexOfObject:sender];
     [self.game chooseCardAtIndex:chooseButtonIndex];
+    [self updateUI];
+}
+
+- (IBAction)touchStartGame:(UIButton *)sender {
+    // Ask for confirmation to init new game
+
+    // Clear out existing game and score
+    self.game = nil;
+    
+    // Update UI
     [self updateUI];
 }
 
